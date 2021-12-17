818004c021f6a646477df7ec7669d0565259a827
Vitaliy Kuznecov <92617216+Tasher2282005@users.noreply.github.com>
2021-12-18 01:38:17 +0800

    Update script.js

--- tutorial_4/script.js
+++ tutorial_4/script.js
@@ -1,9 +1,21 @@
-var a = 2;
-var b = 2;
-var c = a + b;
-var j = false;
-if (a === 2) {
-	console.log('a ravno 2');
+var catNames = ['Kislinka', 'Mandarinka', 'Lisichka', 'Luna', 'kreker'];
+
+var cat= {
+	name: catNames [random(0, catNames.length -1)],
+	color: '#808080',
+	len: 35
+};
+
+var arrayCats = [cat, cat, cat, cat];
+
+console.log(catNames.length-1); 
+console.log(arrayCats.length);
+console.log(random(0, 5));
+
+for (var i = 0; i < arrayCats.length; i++) {
+console.log(arrayCats[i]);
 }
 
-console.log(j);
+function random(min, max) {	
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
